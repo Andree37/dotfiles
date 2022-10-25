f6e0f86ca61a80981cdc0f28aa22d2ccc7031176
Andre <33375224+Andree37@users.noreply.github.com>
2022-10-26 00:30:33 +0100

    bufferline and colorizer

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -29,4 +29,6 @@ packer.startup(function(use)
     use 'nvim-telescope/telescope.nvim'
     use 'nvim-telescope/telescope-file-browser.nvim'
     use 'kyazdani42/nvim-web-devicons' -- File Icons
+    use 'akinsho/nvim-bufferline.lua'
+    use 'norcalli/nvim-colorizer.lua'
 end)
