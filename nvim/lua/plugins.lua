26e5ee5555d8e771da9d5a031588f54245a48166
Andre <33375224+Andree37@users.noreply.github.com>
2022-10-25 01:28:14 +0100

    nvim tmux config

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,21 @@
+local status, packer = pcall(require, 'packer')
+if (not status) then
+    print("Packer is not installed")
+    return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use {
+        "ellisonleao/gruvbox.nvim",
+    }
+    use 'L3MON4D3/LuaSnip' -- Snippet
+    use 'hoob3rt/lualine.nvim' -- Statusline
+    use 'onsails/lspkind-nvim' -- pictograms
+    use 'hrsh7th/cmp-buffer' -- nvimcom source for buffer words
+    use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
+    use 'hrsh7th/nvim-cmp' -- Completion
+    use 'neovim/nvim-lspconfig' -- LSP
+end)
